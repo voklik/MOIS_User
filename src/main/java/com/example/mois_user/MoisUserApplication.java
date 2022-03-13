2000b549cd42f5ce9f152c8ed2e0a6234604c9ee
Michael Souček <michael.soucek1@seznam.cz>
2022-03-13 16:41:28 +0100

    init commit

---
+++ src/main/java/com/example/mois_user/MoisUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.mois_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoisUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoisUserApplication.class, args);
+    }
+
+}
