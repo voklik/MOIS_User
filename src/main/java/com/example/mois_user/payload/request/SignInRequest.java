171142d93e9a95ebe0ba12b679153e9615f445ba
Vilemos <viliproch@seznam.cz>
2022-05-03 18:16:26 +0200

    add constraints

--- src/main/java/com/example/mois_user/payload/request/SignInRequest.java
+++ src/main/java/com/example/mois_user/payload/request/SignInRequest.java
@@ -1,15 +1,16 @@
 package com.example.mois_user.payload.request;
 
 import lombok.Data;
-import javax.validation.constraints.NotEmpty;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class SignInRequest {
 
-    @NotEmpty(message = "Username cannot be empty")
+    @NotBlank(message = "Email nemůže být prázdný")
     private String username;
 
-    @NotEmpty(message = "Password cannot be empty")
+    @NotBlank(message = "Heslo nemůže být prázdné")
     private String password;
 
 }
--- src/main/java/com/example/mois_user/payload/request/SignUpRequest.java
+++ src/main/java/com/example/mois_user/payload/request/SignUpRequest.java
@@ -5,35 +5,37 @@ import com.example.mois_user.annotation.PasswordMatches;
 import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
 @PasswordMatches
 public class SignUpRequest {
 
-    @NotBlank(message = "User email is required")
-    @Email(message = "It should have email format")
+    @NotBlank(message = "Email je povinný")
+    @Email(message = "Špatný formát")
     private String email;
 
     private String phoneNumber;
 
-    @NotEmpty(message = "Please enter your first name")
+    @NotBlank(message = "Vložte své jméno")
     private String firstName;
 
-    @NotEmpty(message = "Please enter your last name")
+    @NotBlank(message = "Vložte své příjmení")
     private String lastName;
 
     @Size(min = 6)
-    @NotEmpty(message = "Password is required")
+    @NotBlank(message = "Heslo je povinné")
     private String password;
 
     private String confirmPassword;
 
+    @NotBlank(message = "Město je povinné")
     private String city;
 
+    @NotBlank(message = "PSČ je povinné")
     private String postCode;
 
+    @NotBlank(message = "Ulice je povinná")
     private String street;
 
 }
